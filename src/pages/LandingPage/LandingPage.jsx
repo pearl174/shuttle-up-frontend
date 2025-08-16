79939cd32f3cf265bbc69d6e16ecf592532f3f9a
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-17 01:49:31 +0530

    Change header to fixed size to remove layout issues. Add zero margin to hero text.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,12 @@
 import "./LandingPage.css";
 
 const LandingPage = () => {
-    return <div className="image-holder"></div>
+    return <div className="image-holder">
+        <div className="hero-content">
+            <h1 className="hero-text">Track Every Rally. Master Every Match.</h1>
+            <button className="primary-btn">Get Started</button>
+      </div>
+    </div>
 }
 
 export default LandingPage;
