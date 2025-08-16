a64ff1cb7815d388ff814ec0ee7d7b0ed8a56fac
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-16 20:15:39 +0530

    Add toggle functionality for theme. Pass them props from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./styles/colors.css";
 import "./styles/base.css";
 import Header from "./components/Header/Header.jsx";
+import { useEffect, useState } from "react";
+
 
 const App = () => {
+  const [theme, toggleTheme] = useState("light");
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme])
   return (
-    <Header />
+    <Header theme={theme} toggleTheme={toggleTheme}/>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,13 @@ import themeIcon from "../../assets/theme-icon.svg";
 import "./Header.css";
 
 const Header = (props) => {
-    const toggleTheme = props.toggleTheme;
+    const { theme, toggleTheme } = props;
 
     return (
         <div className="flex-container">
             <div className="logo">SHUTTLE UP</div>
             <div className="button-container">
-                <button onClick={toggleTheme} className="toggle-theme" aria-label="toggle-theme"><img src={themeIcon} alt="Theme toggle" /></button>
+                <button onClick={() => theme === "light" ? toggleTheme("dark") : toggleTheme("light")} className="toggle-theme" aria-label="toggle-theme"><img src={themeIcon} alt="Theme toggle" /></button>
                 <button className="primary-btn">LOGIN</button>
                 <button className="primary-btn">SIGNUP</button>
             </div>
