0b39a17054bdc9dec4c0ee6870a9f52e7adcba0b
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-17 14:15:25 +0530

    Add navigation to buttons.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import themeIcon from "../../assets/theme-icon.svg";
 import "./Header.css";
 
 const Header = (props) => {
     const { theme, toggleTheme } = props;
+    const navigate = useNavigate();
 
     return (
         <div className="flex-container">
             <div className="logo">SHUTTLE UP</div>
             <div className="button-container">
                 <button onClick={() => theme === "light" ? toggleTheme("dark") : toggleTheme("light")} className="toggle-theme" aria-label="toggle-theme"><img src={themeIcon} alt="Theme toggle" /></button>
-                <button className="primary-btn">LOGIN</button>
-                <button className="primary-btn">SIGNUP</button>
+                <button className="primary-btn" onClick={() => navigate("/login")}>LOGIN</button>
+                <button className="primary-btn" onClick={() => navigate("/signup")}>SIGNUP</button>
             </div>
         </div>
     )
