09760a73b7394fd581b1b8ff620ce461cbf783d3
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-17 13:01:40 +0530

    Add typewriter effect using react library.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,18 @@
 import "./LandingPage.css";
+import { Typewriter } from "react-simple-typewriter";
 
 const LandingPage = () => {
     return <div className="image-holder">
         <div className="hero-content">
-            <h1 className="hero-text">Track Every Rally. Master Every Match.</h1>
+            <h1 className="hero-text">
+                <Typewriter
+                    words={["Track Every Rally. Master Every Match."]}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={80}
+                    deleteSpeed={50}
+                />
+            </h1>
             <button className="primary-btn">Get Started →</button>
       </div>
     </div>
