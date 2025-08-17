1126645ca97e349c47bf53d2152009c7c1938b0e
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-17 14:09:30 +0530

    Add navigation to signup place on button click.

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,9 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import "./LandingPage.css";
 import { Typewriter } from "react-simple-typewriter";
 
 const LandingPage = () => {
+    const navigate = useNavigate();
     return <div className="image-holder">
         <div className="hero-content">
             <h1 className="hero-text">
@@ -13,7 +15,7 @@ const LandingPage = () => {
                     deleteSpeed={50}
                 />
             </h1>
-            <button className="primary-btn">Get Started →</button>
+            <button className="primary-btn" onClick={() => navigate("/signup")}>Get Started →</button>
       </div>
     </div>
 }
