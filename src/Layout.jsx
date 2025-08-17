3d0127d5f08947a3eb2017d2684e0dbadaa991ec
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-17 13:55:59 +0530

    Add App.jsx logic to Layout.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,19 @@
 import Header from "./components/Header/Header.jsx";
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./styles/base.css";
+import "./styles/colors.css";
 
 const Layout = () => {
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+    }, [theme]);
+
     return (
         <>
-            <Header />
+            <Header theme={theme} toggleTheme={setTheme} />
             <Outlet />
         </>
     )
