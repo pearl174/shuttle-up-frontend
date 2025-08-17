5893abb93ae433c35d8d7f33cefab858b451f5f5
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-17 15:01:49 +0530

    Add flex container for form.

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,15 +1,17 @@
 import "./SignUp.css";
 const SignUp = () => {
     return (
-        <form className="signup-form" action="/" method="POST">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" />
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-            <button type="submit" className="primary-btn">Register</button>
-        </form>
+        <div className="flex-container-signup">
+            <form className="signup-form" action="/" method="POST">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id="username" />
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" />
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+                <button type="submit" className="primary-btn register-btn">Register</button>
+            </form>
+        </div>
     )
 }
 
