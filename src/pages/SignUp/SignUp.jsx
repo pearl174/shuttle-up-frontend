77a4bf06a2c3873ec5821299b4a304508e24dd79
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-18 11:47:39 +0530

    Add input validation. Regex and other remaining.

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,11 +4,11 @@ const SignUp = () => {
         <div className="flex-container-signup">
             <form className="signup-form" action="/" method="POST">
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" />
+                <input type="text" name="username" id="username" required placeholder="johndoe" />
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" required placeholder="johndoe@gmail.com" />
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
+                <input type="password" name="password" id="password" required minLength="6" maxLength="20" />
                 <button type="submit" className="primary-btn register-btn">Register</button>
             </form>
         </div>
