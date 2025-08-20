58287703b585a1f856035706b0eaf4c11c0b37dd
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-20 23:37:05 +0530

    Add successful profile data fetching.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,25 @@
 import { useParams } from "react-router-dom";
+import API_BASE from "../../../config.js";
 import "./Profile.css";
 
 const Profile = () => {
     const { username } = useParams();
+    const token = localStorage.getItem("token");
+    const fetchProfile = async () => {
+        const res = await fetch(`${API_BASE}api/profile/${username}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        const data = await res.json();
+        console.log(data);
+    }
+    fetchProfile().catch(() => {
+        console.log("Something went wrong")
+    })
+    
     return (
         <h1>Profile page for {username}</h1>
     )
