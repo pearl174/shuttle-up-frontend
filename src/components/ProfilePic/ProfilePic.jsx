cba7489470f465dfa3a314267f6d9c7b5649f86a
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-28 18:16:19 +0530

    Change default avatar to cat image. Add width and height, and other css to avatar.

--- src/components/ProfilePic/ProfilePic.jsx
+++ src/components/ProfilePic/ProfilePic.jsx
@@ -1,7 +1,7 @@
 import "./ProfilePic.css";
 const ProfilePic = () => {
     return (
-        <div className="profile-container"><img src="./src/assets/theme-icon.png" alt="Shuttle image?" /></div>
+        <div className="profile-container"><img src="./catprofile.png" alt="Shuttle image?" /></div>
     )
 }
 
