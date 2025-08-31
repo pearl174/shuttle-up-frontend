8ba8c3d99b6607eaf0940545cccabecbf565e5e4
pearl-174 <115370971+pearl174@users.noreply.github.com>
2025-08-31 19:29:42 +0530

    Arrange profile dashboard using grid and add appropriate styling. Metrics and heatmap left. Along with play match button.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import API_BASE from "../../../config.js";
 import "./Profile.css";
+import ProfilePic from "../../components/ProfilePic/ProfilePic.jsx";
+import ProgressGraph from "../../components/ProgressGraph/ProgressGraph.jsx";
 
 const Profile = () => {
     const { username } = useParams();
@@ -23,7 +25,23 @@ const Profile = () => {
     
     
     return (
-        <h1>Profile page for {username}</h1>
+        <div className="profile-grid-container">
+            <div className="left-side">
+                <div className="profilepic-bio-container">
+                    <ProfilePic />
+                    <div className="bio-text">If you're gonna hit it, hit it until it breaks!</div>
+                    <button className="primary-btn friends">Friends</button>
+                    <button className="primary-btn friend-requests">Friend Requests</button>
+                </div>
+                <div className="metrics">
+                    Insert Metrics
+                </div>
+            </div>
+            <div className="right-side">
+                <div className="progress-graph-container"><ProgressGraph /></div>
+                <div className="heatmap"></div>
+            </div>
+        </div>
     )
 }
 
