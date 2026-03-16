803c0555457b2555dbfcba275235fc81f697d2c8
pearl-174 <115370971+pearl174@users.noreply.github.com>
2026-03-16 21:19:03 +0530

    Add dummy navigation functions handling button clicks in button tray.

--- src/pages/Profile/ButtonTray.jsx
+++ src/pages/Profile/ButtonTray.jsx
@@ -1,14 +1,27 @@
 import "./Profile.css";
 import "./ButtonTray.css";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
 
 const ButtonTray = () => {
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
 
+    const playMatch = () => {
+        navigate("/playMatch");
+    }
+    const viewFriends = () => {
+        navigate(`/friends/${user}`);
+    }
+    const viewPastMatches = () => {
+        navigate(`/history/${user}`)
+    }
     return (
         <div className="button-tray-container">
-            <button className="primary-btn action-button">Play a Match!</button>
-            <button className="primary-btn action-button">Friends</button>
-            <button className="primary-btn action-button">Friends Requests</button>
-            <button className="primary-btn action-button">View Past Matches</button>
+            <button className="primary-btn action-button" onClick={() => playMatch()}>Play a Match!</button>
+            <button className="primary-btn action-button" onClick={() => viewFriends()}>Friends</button>
+            <button className="primary-btn action-button" onClick={() => viewPastMatches()}>View Past Matches</button>
         </div>
     )
 }
