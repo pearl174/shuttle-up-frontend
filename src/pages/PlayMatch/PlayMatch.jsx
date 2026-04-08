247db32024df0f1d3a4baea42394a9f2e704fe1a
Pearl Sheth <115370971+pearl174@users.noreply.github.com>
2026-04-08 05:10:23 +0000

    Add Play Match component and badminton girl bumping shuttle frames.

---
+++ src/pages/PlayMatch/PlayMatch.jsx
@@ -0,0 +1,7 @@
+import "./PlayMatch.css";
+import "../../assets/frame1.png";
+import "../../assets/frame2.png";
+import "../../assets/frame3.png";
+import "../../assets/frame4.png";
+import "../../assets/frame5.png";
+
