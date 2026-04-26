1061344a91a54e5e5c7953245f7685b716aa4c0d
pearl <115370971+pearl174@users.noreply.github.com>
2026-04-26 09:28:42 +0000

    Fetch actual friends for friend list.

--- src/pages/FriendsPage/FriendList.jsx
+++ src/pages/FriendsPage/FriendList.jsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getFriends } from "../../api/friends";
+import { useParams } from "react-router-dom";
 
 export default function FriendList() {
     const [friends, setFriends] = useState(['john', 'amy', 'hardin']);
     const [searchTerm, setSearchTerm] = useState("");
+    const params = useParams();
 
-    
+    useEffect(() => {
+        const fetchFriends = async() => {
+            const {res, data} = await getFriends(params.username);
+            if (res.status === 401) {
+                alert("Something went wrong. Please login again.");
+                localStorage.removeItem("token");
+                navigate("/login");
+            } else if (res.status === 500) {
+                alert("Something went wrong");
+                navigate(`/profile/${username}`);
+            } else if (res.status === 200) {
+                setFriends(data.friends);
+                console.log("Here are your friends :D.");
+            } else {
+                console.log("lol idk what's happening");
+                navigate(`/profile/${username}`);
+            }
+        }
+        fetchFriends().catch((err) => {
+            console.error(err);
+        });
+    }, []);
+
     return (
         <>
             <input type="text" 
