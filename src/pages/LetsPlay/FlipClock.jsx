339ddd8c6090ba881cb30e4d4ab7d174714dd586
pearl <115370971+pearl174@users.noreply.github.com>
2026-06-10 15:50:12 +0000

    Flipclock animation in progress ...

--- src/pages/LetsPlay/FlipClock.jsx
+++ src/pages/LetsPlay/FlipClock.jsx
@@ -1,11 +1,18 @@
 import "./FlipClock.css";
 
 export default function FlipClock() {
+    const [flip, setFlip] = useState(false);
+    const handleFlip = () => {
+        setFlip(true);
+        const timer = setTimeout(() => {
+            setFlip(false);
+        }, 1000);
+    }
     return (
         <div className="flipclock-container">
             <div className="minutes-card">
                 <div className="card-upper">
-                    <div className="number-upper">11</div>
+                    <div className="number-upper" onClick={handleFlip}>11</div>
                 </div>
                 <div className="card-lower">
                     <div className="number-lower">11</div>
