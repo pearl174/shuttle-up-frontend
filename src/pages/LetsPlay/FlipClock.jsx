f680d3bb47f07e35de0337cec6b534f845c980e8
pearl <115370971+pearl174@users.noreply.github.com>
2026-06-13 07:22:52 +0000

    Upper card seconds animation worksgit add . Transitions in progress...

--- src/pages/LetsPlay/FlipClock.jsx
+++ src/pages/LetsPlay/FlipClock.jsx
@@ -1,23 +1,29 @@
 import "./FlipClock.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function FlipClock() {
     const [flip, setFlip] = useState(false);
-    const [seconds, setSeconds] = useState(() => {
-        return 0;
-    })
-    const handleFlip = () => {
-        setFlip(true);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [startCounting, setStartCounting] = useState(true);
 
-        const timer = setTimeout(() => {
-            setFlip(false);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFlip(true);
         }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+    // }, [startCounting]);
+
+    const onFlipEnd = () => {
+        setSeconds(s => s + 1); 
+        setFlip(false);
     }
     return (
         <div className="flipclock-container">
             <div className="minutes-card">
                 <div className="card-upper">
-                    <div className={`number-upper ${flip ? "flip" : ""}`} onClick={handleFlip}>11</div>
+                    <div className={`number-upper ${flip ? "flip" : ""}`}>11</div>
                 </div>
                 <div className="card-lower">
                     <div className="number-lower">11</div>
@@ -25,9 +31,11 @@ export default function FlipClock() {
             </div>
             <div className="seconds-card">
                 <div className="card-upper">
-                    <div className="number-upper">00</div>
+                    <div className={`number-upper ${flip? "": "invisible"}`}>{seconds + 1}</div>
+                    <div className={`number-upper ${flip? "flip": ""}`}
+                        onTransitionEnd={onFlipEnd}>{seconds}</div>
                 </div>
-                <div className="card-lower">
+                <div className="card-lower invisible">
                     <div className="number-lower">00</div>
                 </div>
             </div>
