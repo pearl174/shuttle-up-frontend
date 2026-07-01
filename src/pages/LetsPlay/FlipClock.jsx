75911bce88783d767c01501cfd56327139f725ce
pearl <115370971+pearl174@users.noreply.github.com>
2026-07-01 04:56:04 +0000

    Finish flip clock. (Add minute card functionality)

--- src/pages/LetsPlay/FlipClock.jsx
+++ src/pages/LetsPlay/FlipClock.jsx
@@ -1,47 +1,52 @@
 import "./FlipClock.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function FlipClock() {
     const [flip, setFlip] = useState(false);
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
+    const [flipMinute, setFlipMinute] = useState(false);
+    const [seconds, setSeconds] = useState(-1);
+    const [minutes, setMinutes] = useState(-1);
+    const secondsRef = useRef(-1);
+    const minutesRef = useRef(-1);
     const [startCounting, setStartCounting] = useState(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
+            if (secondsRef.current === 58) {
+                minutesRef.current = minutesRef.current + 1;
+                setMinutes(minutesRef.current);
+                setFlipMinute(true);
+            }
             setFlip(true);
-            setSeconds((s) => {
-                if (s === 60) {
-                    return 0;
-                } else return s + 1;
-            }); 
+            const nextSec = secondsRef.current === 59 ? 0 : secondsRef.current + 1;
+            secondsRef.current = nextSec;
+            setSeconds(nextSec);
         }, 1000);
         return () => clearInterval(interval);
     }, []);
     // }, [startCounting]);
 
-    const onFlipStart = () => {
-            
-    }
     const onFlipEnd = () => {
         setFlip(false);
+    }
+
+    const onFlipMinEnd = () => {
+        setFlipMinute(false);
     }
 
     return (
         <div className="flipclock-container">
             <div className="minutes-card">
-                <div className="card-upper">
-                    <div className={`number-upper ${flip ? "flip" : ""}`}>11</div>
-                </div>
-                <div className="card-lower">
-                    <div className="number-lower">11</div>
-                </div>
+                <div className={`card-upper`}><div className={`number-upper`}>{minutes + 1 < 10? `0${minutes + 1}` : minutes + 1 === 60? "00" : `${minutes + 1}`}</div></div>
+                <div className={`card-upper ${flipMinute? "flip" : ""} non-static`}><div className={`number-upper`}>{minutes < 10? `0${minutes}` : `${minutes}`}</div></div>
+                <div className="card-lower"><div className={`number-lower`}>{minutes < 10? `0${minutes}` : `${minutes}`}</div></div>
+                <div onAnimationEnd={onFlipMinEnd} className={`card-lower ${flipMinute? "flip" : ""}`}><div className={`number-lower`}>{minutes + 1 < 10? `0${minutes + 1}` : minutes + 1 === 60? "00" : `${minutes + 1}`}</div></div>
             </div>
             <div className="seconds-card">
-                <div className={`card-upper`}><div className={`number-upper`}>{seconds + 1}</div></div>
-                <div className={`card-upper ${flip? "flip" : ""} non-static`}><div className={`number-upper`}>{seconds}</div></div>
-                <div className="card-lower"><div className={`number-lower`}>{seconds}</div></div>
-                <div onAnimationEnd={onFlipEnd} className={`card-lower ${flip? "flip" : ""}`}><div className={`number-lower`}>{seconds + 1}</div></div>
+                <div className={`card-upper`}><div className={`number-upper`}>{seconds + 1 < 10? `0${seconds + 1}` : seconds + 1 === 60? "00" : `${seconds + 1}`}</div></div>
+                <div className={`card-upper ${flip? "flip" : ""} non-static`}><div className={`number-upper`}>{seconds < 10? `0${seconds}` : `${seconds}`}</div></div>
+                <div className="card-lower"><div className={`number-lower`}>{seconds < 10? `0${seconds}` : `${seconds}`}</div></div>
+                <div onAnimationEnd={onFlipEnd} className={`card-lower ${flip? "flip" : ""}`}><div className={`number-lower`}>{seconds + 1 < 10? `0${seconds + 1}` : seconds + 1 === 60? "00" : `${seconds + 1}`}</div></div>
             </div>
         </div>
     )
